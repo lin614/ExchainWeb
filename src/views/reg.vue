92a62f61133a75a15ef8e03bbc3504cec276451d
luchengwei <luchengwei@bubi.cn>
2018-08-09 21:10:59 +0800

    行为验证码切换中英文

--- src/views/reg.vue
+++ src/views/reg.vue
@@ -150,6 +150,11 @@ export default {
       }
     }
   },
+  computed: {
+    activeLang() {
+      return this.$store.state.activeLang
+    }
+  },
   methods: {
     regUser(name) {
       var vu = this
@@ -256,7 +261,8 @@ export default {
               challenge: data.challenge,
               offline: !data.success,
               new_captcha: true,
-              product: 'bind'
+              product: 'bind',
+              lang: this.activeLang ? (this.activeLang === 'cn') ? 'zh-cn' : this.activeLang : 'zh-cn'
             },
             function(captchaObj) {
               vu.geettest = captchaObj
@@ -309,6 +315,7 @@ export default {
     var vu = this
     bus.$on('langChange', () => {
       vu.$refs.regInfo.resetFields()
+      this.initGeetest()
     })
     window.addEventListener('keyup', this.onEnter)
   },
