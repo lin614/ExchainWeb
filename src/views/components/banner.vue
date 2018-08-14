8475f9fdcb5a4bb99f3ed371bd1f326080167b94
陈万安 <chenwanan@exchain.com>
2018-08-14 23:27:52 +0800

    feat: 首页banner、https 【4.5】

--- src/views/components/banner.vue
+++ src/views/components/banner.vue
@@ -14,13 +14,19 @@
           </div>
         </CarouselItem>
         <CarouselItem>
-          <div class="banner_2">
+          <div :class="'banner_2_' + $t('common.imgLang')">
             <block>
-              <div slot="inner">
-                <h2>{{ $t('banner.title2') }}</h2>
-                <hr/>
-                <p class="banner-descript2" v-html="$t('banner.descript2')"></p>
-              </div>
+              <a v-if="$t('common.imgLang') === 'cn'" target="_blank" href="https://support.exchain.com/hc/zh-cn/articles/360013309991" title="Exchain种子测试正式开启，千人限量每日瓜分2000 USDT">
+                <div slot="inner" style="height: 380px;">
+                  <!-- <h2>{{ $t('banner.title2') }}</h2> -->
+                  <!-- <hr/> -->
+                  <!-- <p class="banner-descript2" v-html="$t('banner.descript2')"></p> -->
+                </div>
+              </a>
+              <a v-if="$t('common.imgLang') === 'en'" target="_blank" href="https://support.exchain.com/hc/en-us/articles/360013309991" title="Instructions For Exchain Special User Test">
+                <div slot="inner" style="height: 380px;">
+                </div>
+              </a>
             </block>
           </div>
         </CarouselItem>
@@ -105,9 +111,14 @@ export default {
       background: url(../../static/imgs/ban-bg1.png);
     }
   }
-  .banner_2 {
+  .banner_2_en {
     .block {
-      background: url(../../static/imgs/ban-bg2.png);
+      background: url(../../static/imgs/ban-bg2-en.png);
+    }
+  }
+  .banner_2_cn {
+    .block {
+      background: url(../../static/imgs/ban-bg2-cn.png);
     }
   }
   .notice {
--- src/views/components/notice.vue
+++ src/views/components/notice.vue
@@ -1,14 +1,31 @@
 <template>
     <div class="notice">
         <Card icon="compose" :bordered="false">
-            <p slot="title">{{ $t('index.announcement.title') }} <a class="fr" style="font-weight: 400;" target="_blank" href="https://support.exchain.com">{{ $t('index.announcement.more') }} ></a> </p>
+            <p slot="title">
+                {{ $t('index.announcement.title') }}
+                <a class="fr" style="font-weight: 400;" target="_blank" href="https://support.exchain.com/hc/zh-cn" v-if="$t('index.announcement.more') === '更多'">{{ $t('index.announcement.more') }} ></a>
+                <a class="fr" style="font-weight: 400;" target="_blank" href="https://support.exchain.com/hc/en-us" v-if="$t('index.announcement.more') === 'More'">{{ $t('index.announcement.more') }} ></a>
+            </p>
             <ul>
                 <li>
-                    <a target="_blank" href="https://support.exchain.com/hc/zh-cn/articles/360006935231-Exchain%E8%B6%85%E7%BA%A7%E5%90%88%E4%BD%9C%E4%BC%99%E4%BC%B4%E8%AE%A1%E5%88%92%E5%BC%80%E5%90%AF">
+                    <a class="notice-a" target="_blank" :href="$t('index.announcement.more') === '更多' ? 'https://support.exchain.com/hc/zh-cn/articles/360006935231' : 'https://support.exchain.com/hc/en-us/articles/360006935231'" :title="$t('index.announcement.news')">
                         {{ $t('index.announcement.news') }}
                         <Icon type="chevron-right"></Icon>
                     </a>
                 </li>
+                <li v-if="$t('index.announcement.more') === '更多'">
+                    <a class="notice-a" target="_blank" href="https://support.exchain.com/hc/zh-cn/articles/360013213691" title="种子测试期间平台使用说明">
+                        种子测试期间平台使用说明
+                        <Icon type="chevron-right"></Icon>
+                    </a>
+                </li>
+                <li v-if="$t('index.announcement.more') === 'More'">
+                    <a class="notice-a" target="_blank" href="https://support.exchain.com/hc/en-us/articles/360013213691" title="Instructions For Exchain Special User Test">
+                        Instructions For Exchain Special User Test
+                        <Icon type="chevron-right"></Icon>
+                    </a>
+                </li>
+                
             </ul>
         </Card>
     </div>
@@ -33,8 +50,20 @@ export default {
     cursor: pointer;
   }
   i {
-    float: right;
-    line-height: @font-text*2; 
+    line-height: @font-text*2;
+    position: absolute;
+    right: 11px; 
+  }
+  .ivu-card-body:hover {
+    box-shadow: initial;
+  }
+  .notice-a {
+        display: block;
+        width: 228px;
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        padding-right: 13px;
   }
 }
 </style>
