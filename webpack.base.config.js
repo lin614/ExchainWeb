ebede7fb99b56a59b8e3f43b79a7b38cfe0dc133
林矿坡 <linkuangpo@exchangechain.com>
2018-09-18 16:42:59 +0800

    1,打包方式修改

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -10,7 +10,7 @@ module.exports = {
         vendors: ["babel-polyfill", "./src/vendors.js"]
     },
     output: {
-        path: path.join(__dirname, './dist')
+        path: path.join(__dirname, './dist/dist/')
     },
 
     module: {
